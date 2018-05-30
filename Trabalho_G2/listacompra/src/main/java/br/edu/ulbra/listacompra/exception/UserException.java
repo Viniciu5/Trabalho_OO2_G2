58eab23a2283714344e52a330b879c446bb1d88e
Viniciu5 <39700738+Viniciu5@users.noreply.github.com>
2018-05-30 18:46:03 -0300

    TESTE

---
+++ Trabalho_G2/listacompra/src/main/java/br/edu/ulbra/listacompra/exception/UserException.java
@@ -0,0 +1,10 @@
+package br.edu.ulbra.listacompra.exception;
+
+public class UserException extends Exception {
+
+    public UserException(){}
+
+    public UserException(String message){
+        super(message);
+    }
+}
