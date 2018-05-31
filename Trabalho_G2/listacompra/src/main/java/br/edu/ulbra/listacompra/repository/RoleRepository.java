54b976a385c5106eed54fbd64f49c5da085d88d4
Viniciu5 <39700738+Viniciu5@users.noreply.github.com>
2018-05-30 21:45:14 -0300

    REDONE

--- Trabalho_G2/listacompra/src/main/java/br/edu/ulbra/listacompra/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ulbra.listacompra.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.edu.ulbra.listacompra.model.Role;
-
-public interface RoleRepository extends CrudRepository<Role, Long> {
-}
