e8f7939145c673b6fb656464c5cf12a3f6490955
PWasp <39781887+PWasp@users.noreply.github.com>
2018-06-27 13:49:12 -0300

    Update EventInput.java

--- Trabalho_G2/vinicius.patricia.william/src/main/java/br/edu/ulbra/submissoes/input/EventInput.java
+++ Trabalho_G2/vinicius.patricia.william/src/main/java/br/edu/ulbra/submissoes/input/EventInput.java
@@ -1,9 +1,23 @@
 package br.edu.ulbra.submissoes.input;
 
+import java.time.LocalDate;
+import java.util.Date;
+
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 public class EventInput {
 	private Long id;
 	private String name;
-
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	private Date abertura;
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	private Date fechamento;
+	
 
 	public Long getId() {
 		return id;
@@ -20,4 +34,20 @@ public class EventInput {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public Date getAbertura() {
+		return abertura;
+	}
+
+	public void setAbertura(Date abertura) {
+		this.abertura = abertura;
+	}
+	
+	public Date getFechamento() {
+		return fechamento;
+	}
+
+	public void setFechamento(Date fechamento) {
+		this.fechamento = fechamento;
+	}	
 }
